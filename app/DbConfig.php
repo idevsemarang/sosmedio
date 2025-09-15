76ca1050629fff19de77d728ff79ac5be8a93ea9
Dani Hadi <dani@rectmedia.id>
2025-09-16 02:31:42 +0700

    add log query

--- app/DbConfig.php
+++ app/DbConfig.php
@@ -1,26 +1,30 @@
 <?php
 
-class DbConfig{
+class DbConfig
+{
 
-    public function connection() { 
+    public function connection()
+    {
         $servername = "localhost";
         $username = "root";
         $password = "";
         $dbname = "idev_sosmedio";
-        
+
         $conn = mysqli_connect($servername, $username, $password, $dbname);
 
         if (!$conn) {
             die("Connection failed: "
-            . mysqli_connect_error());
+                . mysqli_connect_error());
         }
 
         return $conn;
     }
 
 
-    public function getRows($sql) 
+    public function getRows($sql)
     {
+        $this->logQuery($sql);
+
         $arrDatas = [];
 
         $conn = $this->connection();
@@ -28,11 +32,11 @@ class DbConfig{
         if (mysqli_query($conn, $sql)) {
             $result = mysqli_query($conn, $sql);
 
-            while($row = $result->fetch_assoc()) {                
+            while ($row = $result->fetch_assoc()) {
                 $arrDatas[] = $row;
             }
-        } 
-        
+        }
+
         mysqli_close($conn);
 
         return $arrDatas;
@@ -41,11 +45,23 @@ class DbConfig{
 
     public function setStatement($sql)
     {
+        $this->logQuery($sql);
+
         $conn = $this->connection();
 
         mysqli_query($conn, $sql);
-        
+
         mysqli_close($conn);
     }
 
+
+    private function logQuery($sql)
+    {
+        $logFile = __DIR__ . '/queryLog/sql_'.date('Y-m-d').'.log';
+
+        $timestamp = date('Y-m-d H:i:s');
+        $logEntry = "[$timestamp] $sql\n";
+
+        file_put_contents($logFile, $logEntry, FILE_APPEND);
+    }
 }
